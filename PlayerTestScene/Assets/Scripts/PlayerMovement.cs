6570fca857b1a27e63f81cafcb3c65b9a902f777
theomaterace <thematerace@gmail.com>
2025-06-19 13:54:01 +0200

    Add Unity project (Assets + ProjectSettings) with PlayerMovement.cs – 19.06.2025

---
+++ PlayerTestScene/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+      rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);  
+    }
+
+}
